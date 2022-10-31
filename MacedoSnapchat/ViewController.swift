dc537968efc6c0c313faa5a5afc4513f94c26981
macedito <aisha.macedo@tecsup.edu.pe>
2022-10-31 14:34:31 -0500

    Initial Commit

---
+++ MacedoSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MacedoSnapchat
+//
+//  Created by Aisha Belen Macedo Zeballos on 31/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
